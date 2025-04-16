cb868a8a9b06e514b8a25b765cc14062008cf507
asiradnan <asiradnan23@gmail.com>
2025-04-16 18:25:10 +0600

    For server

--- CollegePortal/settings.py
+++ CollegePortal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4a+83a$&5_)3wzypak5+tz5p6eun$gg7vazmmq2=ggay@8nhk6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
